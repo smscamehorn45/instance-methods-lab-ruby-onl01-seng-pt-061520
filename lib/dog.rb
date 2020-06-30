86ea9b7111fa52ea15ce539b9de9e27acecc7b1f
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-30 01:34:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,3 +31,37 @@ class Dog
   end 
 end
 fido.bark 
+
+#new line 
+
+class Dog
+end
+ 
+fido = Dog.new
+
+class Dog
+end
+ 
+fido = Dog.new
+fido.methods
+
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end 
+end
+fido.bark 
+
+
+snoopy = Dog.new
+lassie = Dog.new
+snoopy.sit
+lassie.sit 
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end 
+end
+fido.sit
