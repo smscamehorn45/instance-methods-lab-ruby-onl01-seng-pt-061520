24ec66ab70ce66463f4010e11704c7dfa96a98b6
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-30 01:26:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,11 +11,7 @@ end
 fido = Dog.new
 fido.methods
 
-# BUILDING YOUR OWN INSTANCE METHODS
-
-# How do we add our own methods to our classes? In our Dog example, can we teach our Dog a new trick? Can we teach our Dog to bark for example?
-
-# We can. We're used to defining methods already with the def keyword. If we place this method definition within the body of a class, that method becomes a specific behavior of instances of that class, not a generic procedure we can just call whenever we want.
+class, not a generic procedure we can just call whenever we want.
 
 class Dog
   def bark
@@ -23,22 +19,13 @@ class Dog
   end 
 end
 fido.bark 
-# Woof 
 
-# By defining #bark within the Dog class, bark becomes a method of all instances of Dogs. If we make more dogs, they can all bark.
 
 snoopy = Dog.new
 lassie = Dog.new
 snoopy.bark
 lassie.bark 
 
-# Objects can only do what we teach them to do via the code we write and the methods we define. For example, currently, Dogs do not know how to sit.
-
-# snoopy.sit 
-# undefined method `sit' for #<Dog:0x0055fb70ef3398>
-# (repl):70:in `<main>'
-
-# In the same manner, instance methods, the methods that belong to particular instances of particular classes, are not globally evocable like procedural methods. They cannot be called without an instance.class Dog
 class Dog
   def bark
     puts "Woof"
