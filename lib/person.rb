dfd75e1f7a3b8350127371b7649db4e491d3ddc8
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-30 01:41:05 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,65 @@
+class Dog
+end
+ 
+fido = Dog.new
+
+class Dog
+end
+ 
+jon = Person.new
+jon.methods
+
+
+class Person
+  def talk
+    puts "Woof!"
+  end 
+end
+fido.bark 
+
+
+snoopy = Dog.new
+lassie = Dog.new
+snoopy.bark
+lassie.bark 
+
+class Dog
+  def bark
+    puts "Woof!"
+  end 
+end
+fido.bark 
+
+#new line 
+
+class Dog
+end
+ 
+fido = Dog.new
+
+class Dog
+end
+ 
+fido = Dog.new
+fido.methods
+
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end 
+end
+fido.bark 
+
+
+snoopy = Dog.new
+lassie = Dog.new
+snoopy.sit
+lassie.sit 
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end 
+end
+fido.sit
