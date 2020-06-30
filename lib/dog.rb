eacc136fffdbb4a979e16f84976f3c2064e29fcc
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-30 01:57:26 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,67 @@
+
+
+class Dog
+end
+ 
+fido = Dog.new
+
+class Dog
+end
+ 
+fido = Dog.new
+fido.methods
+
+
+class Dog
+  def bark
+    puts "Woof!"
+  end 
+end
+fido.bark 
+
+
+snoopy = Dog.new
+lassie = Dog.new
+snoopy.bark
+lassie.bark 
+
+class Dog
+  def bark
+    puts "Woof!"
+  end 
+end
+fido.bark 
+
+#new line 
+
+class Dog
+end
+ 
+fido = Dog.new
+
+class Dog
+end
+ 
+fido = Dog.new
+fido.methods
+
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end 
+end
+fido.bark 
+
+
+snoopy = Dog.new
+lassie = Dog.new
+snoopy.sit
+lassie.sit 
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end 
+end
+fido.sit
---
+++ lib/person.rb
@@ -0,0 +1,68 @@
+
+
+class Person
+end
+ 
+aaron = Person.new
+
+class Person
+end
+ 
+aaron = Person.new
+aaron.methods
+
+
+class Person
+  def talk
+    puts "Hello World!"
+  end 
+end
+aaron.talk 
+
+
+thrall = Person.new
+anduin = Person.new
+thrall.talk
+anduin.talk 
+
+class Person  
+  def talk
+    puts "Hello World!"
+  end 
+end
+aaron.talk 
+
+#new line 
+
+
+class Person
+end
+ 
+aaron = Person.new
+
+class Person
+end
+ 
+aaron = Person.new
+aaron.methods
+
+
+class Person
+  def walk
+    puts "The Person is walking"
+  end 
+end
+aaron.walk
+
+
+thrall = Person.new
+anduin = Person.new
+thrall.walk
+anduin.walk 
+
+class Person  
+  def walk
+    puts "The Person is walking"
+  end 
+end
+aaron.walk
